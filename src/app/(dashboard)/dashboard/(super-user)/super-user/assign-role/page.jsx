27317cd1c672feda1a68ead0a8d219f37cbb3448
Oliulla <razasm29@gmail.com>
2024-10-18 23:09:46 +0600

    Done assigning role

--- src/app/(dashboard)/dashboard/(super-user)/super-user/assign-role/page.jsx
+++ src/app/(dashboard)/dashboard/(super-user)/super-user/assign-role/page.jsx
@@ -3,7 +3,7 @@ import { envConfig } from "@/lib/envConfig";
 
 export async function getData() {
   const apiUrl = envConfig.url;
-  const res = await fetch(`${apiUrl}/api/dropdown/role-nd-permissions`, {
+  const res = await fetch(`${apiUrl}/api/dropdown/roles-and-users`, {
     cache: "no-store",
   });
   const data = await res.json();
@@ -18,7 +18,5 @@ export default async function page() {
   const endpoint = "/api/user-role";
   const url = apiUrl + endpoint;
 
-  return (
-    <AssignRole users={data?.roles} roles={data?.permissions} url={url} />
-  );
+  return <AssignRole users={data?.users} roles={data?.roles} url={url} />;
 }
