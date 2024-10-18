e3769b28e986e1c8b3d4cc4838cb68e46d13d24c
Oliulla <razasm29@gmail.com>
2024-10-18 21:34:17 +0600

    Create roles and users api for dropdown

---
+++ src/app/api/dropdown/roles-and-users/route.ts
@@ -0,0 +1,60 @@
+/**
+ * @swagger
+ * /api/dropdown/roles-and-users:
+ *   get:
+ *     tags: [Auth]
+ *     description: Fetch all available roles nd users.
+ *     responses:
+ *       200:
+ *         description: Request Success
+ *       500:
+ *         description: Server error.
+ */
+
+import { connectToDatabase } from "@/lib/connectToDb";
+import { responseMessageUtilities } from "@/lib/response.message.utility";
+import { jsonResponse } from "@/lib/response.utils";
+import RoleModel from "@/models/role.model";
+import UserModel from "@/models/user.model";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const [roles, users] = await Promise.all([
+      RoleModel.find({ deletedAt: null }),
+      UserModel.find({ deletedAt: null }),
+    ]);
+
+    const rolesWithLabelValue = roles.map((role) => {
+      return {
+        label: role.rolename,
+        value: role._id,
+      };
+    });
+
+    const usersWithLabelValue = users.map((user) => {
+      return {
+        label: user.name,
+        value: user._id,
+      };
+    });
+
+    const prepareData = {
+      roles: rolesWithLabelValue,
+      users: usersWithLabelValue,
+    };
+
+    // Successful response
+    return jsonResponse(
+      prepareData,
+      responseMessageUtilities.message,
+      responseMessageUtilities.success
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch roles" },
+      { status: 500 }
+    );
+  }
+}
